033f0db866382a1ac223156d23ccb269fff27fe0
y-hokoyama <y-hokoyama@example.com>
2018-02-04 19:49:18 +0900

    fix:jQury周りの読み込み変更

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,13 +1,15 @@
 <html lang="ja">
 <head>
 	<meta charset="utf-8" />
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap-theme.min.css">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700">
-	<link rel="stylesheet" href="css/login.css">
 </head>
-<body>
+<body class="nav-md">
+	<div lass="container body">
+
+	</div>
 
 @yield('content')
 
